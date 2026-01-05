9338e13b67d1c99bb6e9f69f4c9eed532facd365
ucanfindme18 <39616391+ucanfindme18@users.noreply.github.com>
2026-01-05 11:35:35 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'dailyflix-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',  // Assuming your HTML is saved as index.html
+  'https://i.imgur.com/izPYpmZ.png',  // Cache logo
+  // Add more static assets here, e.g., '/styles.css' if you extract styles to a file
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
